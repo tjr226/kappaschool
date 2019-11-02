8dc194746bc7a564f750e621d3bc1dea04f574d4
tjr226 <tjr226@gmail.com>
2019-11-02 18:22:12 -0400

    wiring up data for MemoryGraph component in preparation for Trig demo

--- front-end/src/Components/lectureComponents/MemoryGraph.js
+++ front-end/src/Components/lectureComponents/MemoryGraph.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { getUserCards } from '../../Actions';
+// import { getUserCards } from '../../Actions';
 
 import styled from 'styled-components';
 
 class MemoryGraph extends React.Component {
 
-
-    componentDidMount() {
-
-    };
-
     render() {
+        let unhiddenLectureCards = this.props.userLectureCards.filter(card => card.hidden_boolean === 0)
+        let hiddenLectureCards = this.props.userLectureCards.filter(card => card.hidden_boolean === 1)
+
         return (
             <div>
                 <h2>Graph Placeholder</h2>
                 <p>PLACEHOLDER wait to improve until quiz sections are working</p>
-
-     
+                <p>Unhidden cards: {unhiddenLectureCards.length}</p>
+                <p>Hidden cards: {hiddenLectureCards.length}</p>
             </div>
         )
     }
@@ -26,11 +24,11 @@ class MemoryGraph extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        userCardsList: state.cardReducer.cardList,
+        userLectureCards: state.cardReducer.userLectureCards,
     };
 }
 
 export default connect(
     mapStateToProps,
-    { getUserCards }
+    null,
 )(MemoryGraph)
