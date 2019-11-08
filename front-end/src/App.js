a76ec1cf698f56978d8150ae500195983b43bd0c
tjr226 <tjr226@gmail.com>
2019-11-08 13:09:19 -0500

    refactored Router process for AuthComponents, Login and Signup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,10 +6,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Components/authComponents/Login';
 import Signup from './Components/authComponents/Signup';
 import AuthComponent from './Components/authComponents/AuthComponent.js';
-// import Trig from './Components/trigComponents/Trig.js';
-// import Lecture from './Components/lectureComponents/Lecture.js';
 import ReadingComponent from './Components/readingComponents/ReadingHome.js';
-
 
 import PrivateRoute from './PrivateRoute';
 
@@ -18,10 +15,9 @@ class App extends React.Component {
   render() {  
       return (
         <Router>
-          {/* <Route exact path="/" component={ Login } /> */}
-          {/* <Route exact path="/login" component={Login} /> */}
-          {/* <Route exact path="/signup" component={Signup} /> */}
           <Route exact path="/" component={ AuthComponent } />
+          <Route exact path="/login" component={ AuthComponent } />
+          <Route exact path="/signup" component={ AuthComponent } />
           <PrivateRoute exact path="/reading" component={ReadingComponent} />
         </Router>
       );
