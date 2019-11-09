21cacb5f6d80df073af9757d764b389e3743fb2b
tjr226 <tjr226@gmail.com>
2019-11-08 19:14:23 -0500

    redid Migration to add spacedRepetition tables

--- data/migrations/20191024163038_001.js
+++ data/migrations/20191024163038_001.js
@@ -4,9 +4,9 @@ exports.up = function (knex) {
     return knex.schema
         .createTable('users', users => {
             users.increments();
-            users.string('email', 128).notNullable();
+            users.string('email', 128).notNullable().unique();
             users.string('password', 256).notNullable();
-            users.unique('email');
+            // users.unique('email');
         })
         .createTable('classes', classes => {
             classes.increments();
@@ -44,22 +44,48 @@ exports.up = function (knex) {
                 .unsigned()
                 .notNullable()
                 .references('id')
-                .inTable('lecture_segments')
+                .inTable('lecture_segments');
+        })
+        .createTable('spaced_repetition_patterns', patterns => {
+            patterns.increments();
+            patterns.string('name').notNullable();
+        })
+        .createTable('spaced_repetition_pattern_days', days => {
+            days.increments();
+            days.integer('days').unsigned().notNullable();
+            days.integer('spaced_repetition_pattern')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('spaced_repetition_patterns');
         })
         .createTable('user_cards', user_cards => {
             user_cards.increments()
             user_cards.integer('user_id')
+                // user id FK
                 .unsigned()
                 .notNullable()
                 .references('id')
                 .inTable('users')
             user_cards.integer('card_id')
+                // card id FK
                 .unsigned()
                 .notNullable()
                 .references('id')
                 .inTable('cards')
+            user_cards.integer('spaced_repetition_pattern')
+                // spaced repetition pattern FK
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('patterns')
+            user_cards.integer('previous_spaced_repetition_days')
+                // tracks the amount of days a card was "pushed"
+                // this should probably start at zero for every card
+                .unsigned()
+                .notNullable()
             user_cards.boolean('hidden_boolean').notNullable()
-            user_cards.integer('unix_timestamp')
+            user_cards.integer('next_date_to_review_unix_timestamp')
                 .unsigned()
                 .notNullable()
         })
@@ -68,6 +94,8 @@ exports.up = function (knex) {
 exports.down = function (knex) {
     return knex.schema
         .dropTableIfExists('user_cards')
+        .dropTableIfExists('spaced_repetition_pattern_days')
+        .dropTableIfExists('spaced_repetition_patterns')
         .dropTableIfExists('cards')
         .dropTableIfExists('lecture_segments')
         .dropTableIfExists('lectures')
