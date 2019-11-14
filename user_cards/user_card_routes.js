3d59ff16420abbaff14438a1c54be7340aee6808
tjr226 <tjr226@gmail.com>
2019-11-14 13:21:10 -0500

    deleting unused functions from userCardRoutes and userCardsModel

--- user_cards/user_card_routes.js
+++ user_cards/user_card_routes.js
@@ -14,47 +14,6 @@ router.get('/', (req, res) => {
         })
 })
 
-// get Lectures by Class Id
-router.get('/class/:class_id/findLectures', (req, res) => {
-    const class_id = req.params.class_id;
-    UserCards.findLecturesByClassId(class_id)
-        .then(lectures => {
-            res.status(200).json(lectures);
-        })
-        .catch(error => {
-            res.status(500).json({ errorMessage: "Could not get lectures by class id." })
-        })
-})
-
-
-// get Lecture Segments by Lecture Id
-router.get('/lecture/:lecture_id/findLectureSegments', (req, res) => {
-    const lecture_id = req.params.lecture_id;
-    UserCards.findLectureSegmentsByLectureId(lecture_id)
-        .then(lecture_segments => {
-            res.status(200).json(lecture_segments);
-        })
-        .catch(error => {
-            res.status(500).json({ errorMessage: "Could not get lecture segments by lecture id." })
-        })
-})
-
-
-// hide UserCards
-router.put('/:user_card_id/lecture/:lecture_id/hideCard', (req, res) => {
-    const lecture_id = req.params.lecture_id;
-    const user_card_id = req.params.user_card_id;
-    const user_id = req.user.user_id;
-    const changes = { hidden_boolean: 1 };
-    UserCards.hideCard(changes, user_card_id, user_id, lecture_id)
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        })
-    })
-
 router.put('/:user_card_id/increaseCardTime', (req, res) => {
     const user_card_id = req.params.user_card_id;
     UserCards.increaseCardTime(user_card_id)
@@ -89,21 +48,6 @@ router.get('/currentAndPreviousCardsForLectureSegment/:lecture_segment_id', (req
     const user_id = req.user.user_id
     // console.log("user_id in current and prev is", user_id)
     UserCards.getCurrentAndPreviousCardsForLectureSegment(lecture_segment_id, user_id)
-        .then(cards => {
-            res.status(200).json(cards);
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json(error)
-        })
-})
-
-// ADD CARDS FROM LECTURE SEGMENT TO USER
-// CHANGE THIS TO PARAMS
-router.post('/addLectureSegmentCards', (req, res) => {
-    const user_id = req.body.user_id;
-    const lecture_segment_id = req.body.lecture_segment_id;
-    UserCards.addLectureSegmentCardsToUser(lecture_segment_id, user_id)
         .then(cards => {
             res.status(200).json(cards);
         })
--- user_cards/user_cards_model.js
+++ user_cards/user_cards_model.js
@@ -3,17 +3,13 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
     add,
-    addLectureSegmentCardsToUser,
     find,
     findByUserCardId,
     findByLectureId,
-    findUnhiddenCardsByUserId,
     hideCard,
     increaseCardTime,
     decreaseCardTime,
     getCurrentAndPreviousCardsForLectureSegment,
-    findLecturesByClassId,
-    findLectureSegmentsByLectureId,
     update,
     remove
 }
@@ -25,19 +21,6 @@ function find() {
 async function add(user_card) {
     await db('user_cards').insert(user_card);
     return findByUserId(user_card.user_id);
-}
-
-async function addLectureSegmentCardsToUser(lecture_segment_id, user_id) {
-    cards = await db('cards').where('lecture_segment_id', lecture_segment_id)
-    for (i = 0; i < cards.length; i++) {
-        new_user_card = {};
-        new_user_card.user_id = user_id;
-        new_user_card.card_id = cards[i].id
-        new_user_card.hidden_boolean = 0
-        new_user_card.unix_timestamp = 0
-        await db('user_cards').insert(new_user_card)
-    }
-    return cards
 }
 
 async function increaseCardTime(user_card_id) {
@@ -89,22 +72,6 @@ function findByUserCardId(id) {
         .first();
 }
 
-function findUnhiddenCardsByUserId(user_id) {
-    return db('user_cards')
-        .where('user_id', user_id)
-        .where('hidden_boolean', 0)
-}
-
-function findLecturesByClassId(class_id) {
-    return db('lectures')
-        .where('class_id', class_id)
-}
-
-function findLectureSegmentsByLectureId(lecture_id) {
-    return db('lecture_segments')
-        .where('lecture_id', lecture_id)
-}
-
 function getCurrentAndPreviousCardsForLectureSegment(lecture_segment_id, user_id) {
     return db('user_cards').join('cards', 'user_cards.card_id', 'cards.id')
         .where('user_id', user_id)
@@ -121,26 +88,6 @@ function findByLectureId(lecture_id, user_id) {
         .select([
             'user_cards.id as user_card_id', 'user_cards.user_id', 'user_cards.card_id', 'user_cards.hidden_boolean', 'user_cards.next_date_to_review_unix_timestamp',
             'user_cards.spaced_repetition_pattern', 'user_cards.previous_spaced_repetition_days',
-            'cards.id as card_id', 'cards.question as card_question', 'cards.answer as card_answer', 'cards.lecture_segment_id as lecture_segment_id',
-            'lecture_segments.id as lecture_segment_id', 'lecture_segments.lecture_id as lecture_id'
-        ])
-        .join('cards', 'user_cards.card_id', 'cards.id')
-        .join('lecture_segments', 'cards.lecture_segment_id', 'lecture_segments.id')
-        .where('user_id', user_id)
-        .where('lecture_id', lecture_id)
-}
-
-// this function was built instead of using the update() function to hide individual cards
-// first section is copying the update() functionality to modify the database
-// second section is to return the same results as "find by lecture id"
-// main change from update... need to add lecture_id to the hide_card function
-// in this function, id is the user_card id
-async function hideCard(changes, id, user_id, lecture_id) {
-    await db('user_cards')
-        .where({ id })
-        .update(changes)
-    return db('user_cards')
-        .select(['user_cards.id as user_card_id', 'user_cards.user_id', 'user_cards.card_id', 'user_cards.hidden_boolean', 'user_cards.unix_timestamp',
             'cards.id as card_id', 'cards.question as card_question', 'cards.answer as card_answer', 'cards.lecture_segment_id as lecture_segment_id',
             'lecture_segments.id as lecture_segment_id', 'lecture_segments.lecture_id as lecture_id'
         ])
