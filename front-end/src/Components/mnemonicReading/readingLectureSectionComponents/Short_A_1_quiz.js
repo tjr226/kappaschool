5695b2222d944489d78b4e8af0e3d638a85ce5cd
tjr226 <tjr226@gmail.com>
2019-11-13 19:20:26 -0500

    buttons working to advance between cards

--- front-end/src/Components/mnemonicReading/readingLectureSectionComponents/Short_A_1_quiz.js
+++ front-end/src/Components/mnemonicReading/readingLectureSectionComponents/Short_A_1_quiz.js
@@ -13,6 +13,10 @@ const ShortA1QuizDiv = styled.div``
 const QuizDiv = styled.div``
 const QuizShowingDiv = styled.div``
 
+const NotRememberedButton = styled.button``
+const RememberedButton = styled.button``
+
+
 class ShortA1Quiz extends React.Component {
     constructor(props) {
         super(props);
@@ -23,15 +27,14 @@ class ShortA1Quiz extends React.Component {
             card_number: 0,
             card_list: [],
         }
-        // this.increaseCardNumberRemembered = this.increaseCardNumberRemembered.bind(this)s
-        // this.increaseCardNumberDidNotRemember = this.increaseCardNumberDidNotRemember.bind(this)
+        this.increaseCardNumberRemembered = this.increaseCardNumberRemembered.bind(this)
+        this.increaseCardNusmberDidNotRemember = this.increaseCardNumberDidNotRemember.bind(this)
     }
 
     // componentDidMount() {
     //     }
     showQuiz = e => {
         e.preventDefault();
-        // this.props.getCardsForLectureSection(this.state.lecture_segment_id);
         this.props.getCurrentPrevCardsLectureSection(this.state.lecture_segment_id)
         this.setState({
             ...this.state,
@@ -39,30 +42,58 @@ class ShortA1Quiz extends React.Component {
         })
     }
 
-    // placeholder - increase card number, didn't remember
-    
-    // placeholder - increase card number, remembered
+    increaseCardNumberDidNotRemember = user_card_id => e => {
+        e.preventDefault()
+        this.setState({
+            ...this.state,
+            card_number: this.state.card_number + 1
+        })
+    }
+
+    increaseCardNumberRemembered = user_card_id => e => {
+        e.preventDefault()
+        this.setState({
+            ...this.state,
+            card_number: this.state.card_number + 1
+        })
+    }
 
     render() {
         const cardsList = this.props.card_list
-        console.log(cardsList)
+        // console.log(cardsList)
 
         return (
-        <ShortA1QuizDiv>
-            <h3>Short A 1 Quiz</h3>
-            <QuizDiv>
-                {this.state.show_quiz
-                ?
-                <QuizShowingDiv>
-                    <p>Quiz is showing</p>
-                    <CardDisplay key={this.state.card_number} card={cardsList[this.state.card_number]} />
-                </QuizShowingDiv>
-                :
-                <h5 onClick={this.showQuiz}>Click here to start quiz</h5>
-                }
+            <ShortA1QuizDiv>
+                <h3>Short A 1 Quiz</h3>
+                <QuizDiv>
+                    {this.state.show_quiz
+                        ?
+                        <QuizShowingDiv>
+                            {this.state.card_number < cardsList.length
+                                ?
+                                <div>
+                                    <CardDisplay key={this.state.card_number} card={cardsList[this.state.card_number]} />
+                                    <NotRememberedButton
+                                        className="btn btn-secondary"
+                                        onClick={this.increaseCardNumberDidNotRemember(cardsList[this.state.card_number].id)}>
+                                        Didn't remember
+                                    </NotRememberedButton>
+                                    <RememberedButton
+                                        className="btn btn-secondary"
+                                        onClick={this.increaseCardNumberRemembered(cardsList[this.state.card_number].id)}>
+                                            Remembered
+                                        </RememberedButton>
+                                </div>
+                                :
+                                <h5>section review complete</h5>
+                            }
+                        </QuizShowingDiv>
+                        :
+                        <h5 onClick={this.showQuiz}>Click here to start quiz</h5>
+                    }
 
-            </QuizDiv>
-        </ShortA1QuizDiv>
+                </QuizDiv>
+            </ShortA1QuizDiv>
         )
     }
 }
